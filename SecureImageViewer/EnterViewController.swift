73de53de3fe88dc4fa999ae85a5d4abd69656ffb
Pigeonva <77341488+Pigeonva@users.noreply.github.com>
2022-09-27 22:18:09 +0300

    feat: create enter controller UI and bind

---
+++ SecureImageViewer/EnterViewController.swift
@@ -0,0 +1,41 @@
+//
+//  EnterViewController.swift
+//  SecureImageViewer
+//
+//  Created by Артур Фомин on 27.09.2022.
+//
+
+import UIKit
+
+class EnterViewController: UIViewController {
+    
+    //MARK: - IBOutlets
+    
+    @IBOutlet weak var greetingLabel: UILabel!
+    @IBOutlet weak var textField: UITextField!
+    
+    //MARK: - let/var
+    
+    
+    //MARK: - Lifecycle methods
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        
+    }
+
+    //MARK: - IBActions
+
+    @IBAction func enterTapped(_ sender: UIButton) {
+    }
+    
+    @IBAction func registerTapped(_ sender: UIButton) {
+    }
+    
+    //MARK: - private methods
+    
+    
+    
+}
+
